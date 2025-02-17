260e0623ec41a1a6d02ad01b49149678e424521c
Vas Tregubov <vastregubov@gmail.com>
2025-02-17 13:25:18 +0300

    Первая загрузка кода

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Header } from './components/Header';
+import { SurveyForm } from './components/SurveyForm';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <SurveyForm />
+    </div>
+  );
+};
+
+export default App;
